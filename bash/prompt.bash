05278ce92cd89a4f97682ea2ee38316853368546
brujoand <anders@brujordet.no>
2015-08-14 18:32:03 +0200

    Making colors in prompt more dynamic

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -2,30 +2,29 @@
 # Super awesome kick ass prompt #
 #################################
 
+_color_host_fg="250"
+_color_host_bg="238"
+_color_path_fg="15"
+_color_path_bg="31"
+_color_path_sep="244"
+_color_git_fg="238"
+_color_git_bg="148"
+_color_time_fg="238"
+
 # Colors named by foreground_background
-function ansi_colors() { # prints ansi escape codes for fg and bg (optional)
+function print_color() { # prints ansi escape codes for fg and bg (optional)
   [[ -n "$2" ]] && echo -e "\[\e[38;5;${1}m\e[48;5;${2}m\]" && return
   [[ -n "$1" ]] && echo -e "\[\e[38;5;${1}m\]"
 }
 
-_color_lgrey_grey=$(ansi_colors "250" "238")
-_color_grey_magenta=$(ansi_colors "238" "31")
-_color_white_magenta=$(ansi_colors "15" "31")
-_color_lgray_magenta=$(ansi_colors "244" "31")
-_color_magenta_green=$(ansi_colors "31" "148")
-_color_gray_green=$(ansi_colors "237" "148")
-_color_green=$(ansi_colors "148")
-_color_dblue=$(ansi_colors "31")
-_color_grey=$(ansi_colors "238")
 _color_reset='\[\e[00m\]'
-
 
 # Not using powerline font if this file exists
 function _prompt_generate_chars() {
   if [[ -f "$HOME/.disable_powerline_prompt" ]]; then
     _prompt_segment_char=" "
     _prompt_path_char="/"
-    _prompt_ready_char="->"
+    _prompt_ready_char=">"
   else
     _prompt_segment_char=$'\uE0B0'
     _prompt_path_char=$'\uE0B1'
@@ -69,38 +68,53 @@ function _prompt_generate_git_status() {
 # Filling up the segments of the prompt
 function _prompt_generate_git {
   if [[ -z "$_prompt_git_status" ]]; then
-    _prompt_git="$_color_reset$_color_dblue$_prompt_segment_char"
+    local path_color=$(print_color "$_color_path_bg")
+    _prompt_git="$_color_reset$path_color$_prompt_segment_char"
   else
-    _prompt_git="$_color_magenta_green$_prompt_segment_char$_color_gray_green $_prompt_git_status $_color_reset$_color_green$_prompt_segment_char"
+    local path_git_color=$(print_color "$_color_path_bg" "$_color_git_bg")
+    local git_color=$(print_color "$_color_git_fg" "$_color_git_bg")
+    local git_end_color=$(print_color "$_color_git_bg")
+    _prompt_git="$path_git_color$_prompt_segment_char$git_color $_prompt_git_status $_color_reset$git_end_color$_prompt_segment_char"
   fi
 }
 
+
 function _prompt_generate_path {
-  local sep=$_color_lgray_magenta$_prompt_path_char$_color_white_magenta
-  wdir=$(pwd | sed "s|$HOME|~|")
-  _prompt_path="$_color_grey_magenta$_prompt_segment_char$_color_white_magenta ${wdir//\// $sep }"
+  local host_path_color=$(print_color "$_color_host_bg" "$_color_path_bg")
+  local path_color=$(print_color "$_color_path_fg" "$_color_path_bg")
+  local host_sep_color=$(print_color "$_color_path_sep" "$_color_path_bg")
+  local sep=$host_sep_color$_prompt_path_char$path_color
+  local wdir=$(pwd | sed "s|$HOME|~|")
+  _prompt_path="$host_path_color$_prompt_segment_char$path_color ${wdir//\// $sep }"
+}
+
+function _prompt_generate_host {
+  local host_color=$(print_color "$_color_host_fg" "$_color_host_bg")
+  _prompt_host="$host_color \u@\h"
 }
 
 function _prompt_generate_filler {
-  local left_prompt right_prompt columns fillsize spaces
-
+  local left_prompt right_prompt columns fillsize spaces wdir needed
+  wdir=$(pwd | sed "s|$HOME|~|")
   left_prompt=" $(whoami)@$(hostname -s) ; ${wdir//\// / } ; "
   [[ -n "$_prompt_git_status" ]] && left_prompt+="$_prompt_git_status ; "
   right_prompt="[last: ${_prompt_time_m}m ${_prompt_time_s}s][$(date +%H:%M:%S)]"
   columns=$(tput cols)
-  fillsize=$(( columns - ${#left_prompt} - ${#right_prompt} ))
-
+  needed=$(( ${#left_prompt} + ${#right_prompt} ))
+  fillsize=$(( columns - needed ))
   spaces=$(printf ' %.0s' {1..400})
-  _prompt_filler=$_color_reset${spaces:0:$fillsize}
+  _prompt_filler="$_color_reset${spaces:0:$fillsize}"
 }
 
 function _prompt_generate_time {
-  _prompt_stop_timer
-  _prompt_time="$_color_grey[last: ${_prompt_time_m}m ${_prompt_time_s}s][\t]"
+  _prompt_stop_timer 
+  local time_color=$(print_color "$_color_time_fg")
+  _prompt_time="$time_color[last: ${_prompt_time_m}m ${_prompt_time_s}s][\t]"
 }
 
 # The applying of our prompt
 function set_prompt {
+  _prompt_generate_host
   _prompt_generate_chars
   _prompt_generate_time
   _prompt_generate_path
@@ -108,7 +122,7 @@ function set_prompt {
   _prompt_generate_git
   _prompt_generate_filler
 
-  PS1="\n$_color_lgrey_grey \u@\h $_prompt_path $_prompt_git$_prompt_filler$_prompt_time\n $_prompt_ready_char$_color_reset "
+  PS1="\n$_prompt_host $_prompt_path $_prompt_git$_prompt_filler$_prompt_time\n $_prompt_ready_char$_color_reset "
 }
 
 [[ "$PROMPT_COMMAND" == *set_prompt* ]] ||  export PROMPT_COMMAND="set_prompt;$PROMPT_COMMAND"
