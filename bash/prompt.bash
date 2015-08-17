54fdf7bd599c35ff284894aabc8ed806ea6b31ab
brujoand <anders@brujordet.no>
2015-08-17 07:15:54 +0200

    Friendlier formatting for prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -2,37 +2,76 @@
 #   Simple Bash Prompt (SBP)    #
 #################################
 
+#### Settings ####
+
+# Alerts user if command takse longer than 'n' minutes
+# Comment the line out to disable
 _prompt_alert_threshold=1
+# Do not generate alerts for the following programs:
 _prompt_alert_ignore="vim;ssh;screen;irssi;"
 
-_color_host_fg="250"
-_color_host_bg="238"
-_color_path_fg="15"
-_color_path_bg="31"
-_color_path_sep="244"
-_color_git_fg="238"
-_color_git_bg="148"
-_color_time_fg=$_color_host_fg
-_color_time_bg=$_color_host_bg
-_color_last_fg=$_color_host_bg
-_color_last_bg=$_color_host_fg
-_color_filler_fg=0
-_color_filler_bg=0
-_color_prompt_ready=$_color_last_fg
+_color_host_fg="250" # host text color
+_color_host_bg="238" # host background color
+_color_path_fg="15" # path text color
+_color_path_bg="31" # path background color
+_color_path_sep="244" # path seperator color
+_color_git_fg="238" # git text color
+_color_git_bg="148" # git background color
+_color_time_fg="250" # clock text color
+_color_time_bg="238" # clock background color
+_color_last_fg="238" # last command time text color
+_color_last_bg="250" # last command time background color
+_color_filler_fg=0 # the text color of the empty space (lol)
+_color_filler_bg=0 # the background color of the empty space
+_color_prompt_ready="238" # the color of the prompt character
 
+
+#### End of settings ###
+
+
+####################
+# Color management #
+####################
+
+function print_color() { # prints ansi escape codes for fg and bg (optional)
+  [[ -n "$2" ]] && echo -e "\[\e[38;5;${1}m\e[48;5;${2}m\]" && return
+  [[ -n "$1" ]] && echo -e "\[\e[38;5;${1}m\]"
+}
+
+_color_reset='\[\e[00m\]'
 
 if [[ "${USER}" == "root" ]]; then
   _color_host_fg="0"
   _color_host_bg="1"
 fi
 
-# Colors named by foreground_background
-function print_color() { # prints ansi escape codes for fg and bg (optional)
-  [[ -n "$2" ]] && echo -e "\[\e[38;5;${1}m\e[48;5;${2}m\]" && return
-  [[ -n "$1" ]] && echo -e "\[\e[38;5;${1}m\]"
+##################
+# Helper methods #
+##################
+
+function prompt_toggle_powerline() { # Enable/Disable the use of powerline font in prompt
+  if [[ -f "$HOME/.disable_powerline_prompt" ]]; then
+    rm "$HOME/.disable_powerline_prompt"
+  else
+    touch "$HOME/.disable_powerline_prompt"
+  fi
 }
 
-function _prompt_alert() { # Notification for osx only atm
+function _prompt_generate_chars() { # Not using powerline font if this file exists
+  if [[ -f "$HOME/.disable_powerline_prompt" ]]; then
+    _prompt_segment_char=" "
+    _prompt_path_char="/"
+    _prompt_ready_char=">"
+    _prompt_segrev_char=" "
+  else
+    _prompt_segment_char=$'\uE0B0'
+    _prompt_path_char=$'\uE0B1'
+    _prompt_ready_char=$'\u279C'
+    _prompt_segrev_char=$'\uE0B2'
+  fi
+}
+
+function _prompt_alert() { # User notification
   [[ -z "$2" ]] && echo "I need a title and a message" && return
 
   title=$1
@@ -47,34 +86,7 @@ function _prompt_alert() { # Notification for osx only atm
   fi
 }
 
-_color_reset='\[\e[00m\]'
-
-# Not using powerline font if this file exists
-function _prompt_generate_chars() {
-  if [[ -f "$HOME/.disable_powerline_prompt" ]]; then
-    _prompt_segment_char=" "
-    _prompt_path_char="/"
-    _prompt_ready_char=">"
-    _prompt_segrev_char=" "
-  else
-    _prompt_segment_char=$'\uE0B0'
-    _prompt_path_char=$'\uE0B1'
-    _prompt_ready_char=$'\u279C'
-    _prompt_segrev_char=$'\uE0B2'
-  fi
-}
-
-
-function prompt_toggle_powerline() { # Enable/Disable the use of powerline font in prompt
-  if [[ -f "$HOME/.disable_powerline_prompt" ]]; then
-    rm "$HOME/.disable_powerline_prompt"
-  else
-    touch "$HOME/.disable_powerline_prompt"
-  fi
-}
-
-# Timer
-function _prompt_start_timer {
+function _prompt_start_timer { # Timer of last command
   _prompt_timer=${_prompt_timer:-$SECONDS}
 }
 
@@ -87,37 +99,22 @@ function _prompt_stop_timer {
 
 trap '_prompt_start_timer' DEBUG
 
-function _prompt_generate_git_status() {
-  if [[ -n "$(git rev-parse --git-dir 2> /dev/null)" ]]; then
-    local git_head git_state
-    git_head=$(sed -e 's,.*/\(.*\),\1,' <(git symbolic-ref HEAD 2>/dev/null || git rev-parse --short HEAD))
-    git_state=" $(git status --porcelain | sed -Ee 's/^(.M|M.|.R|R.) .*/\*/' -e 's/^(.A|A.) .*/\+/' -e 's/^(.D|D.) .*/\-/' | grep -oE '^(\*|\+|\?|\-)' | sort -u | tr -d '\n')"
-    _prompt_git_status=$git_head$git_state
-  else
-    _prompt_git_status=""
-  fi
-}
+#############################
+# Prompt segment generation #
+#############################
 
-# Filling up the segments of the prompt
-function _prompt_generate_git {
-  if [[ -z "$_prompt_git_status" ]]; then
-    local -r path_color=$(print_color "$_color_path_bg")
-    _prompt_git="$_color_reset$path_color$_prompt_segment_char"
-  else
-    local -r path_git_color=$(print_color "$_color_path_bg" "$_color_git_bg")
-    local -r git_color=$(print_color "$_color_git_fg" "$_color_git_bg")
-    local -r git_end_color=$(print_color "$_color_git_bg")
-    _prompt_git="$path_git_color$_prompt_segment_char$git_color $_prompt_git_status $_color_reset$git_end_color$_prompt_segment_char"
+function _prompt_generate_env {
+  local env
+  if [[ -n "$SSH_CLIENT" ]]; then
+    env="ssh"
   fi
-}
-
-function _prompt_generate_path {
-  local -r host_path_color=$(print_color "$_color_host_bg" "$_color_path_bg")
-  local -r path_color=$(print_color "$_color_path_fg" "$_color_path_bg")
-  local -r host_sep_color=$(print_color "$_color_path_sep" "$_color_path_bg")
-  local -r sep=$host_sep_color$_prompt_path_char$path_color
-  local -r wdir=$(pwd | sed "s|$HOME|~|")
-  _prompt_path="$host_path_color$_prompt_segment_char$path_color ${wdir//\// $sep }"
+  if [[ -n "$STY" ]]; then
+    if [[ -n "$env" ]]; then
+      env=+"+screen"
+    else
+      env="screen"
+    fi
+  fi
 }
 
 function _prompt_generate_host {
@@ -131,17 +128,35 @@ function _prompt_generate_host {
   _prompt_host="$host_color $_prompt_host_value"
 }
 
-function _prompt_generate_env {
-  local env
-  if [[ -n "$SSH_CLIENT" ]]; then
-    env="ssh"
+function _prompt_generate_path {
+  local -r host_path_color=$(print_color "$_color_host_bg" "$_color_path_bg")
+  local -r path_color=$(print_color "$_color_path_fg" "$_color_path_bg")
+  local -r host_sep_color=$(print_color "$_color_path_sep" "$_color_path_bg")
+  local -r sep=$host_sep_color$_prompt_path_char$path_color
+  local -r wdir=$(pwd | sed "s|$HOME|~|")
+  _prompt_path="$host_path_color$_prompt_segment_char$path_color ${wdir//\// $sep }"
+}
+
+function _prompt_generate_git_status() {
+  if [[ -n "$(git rev-parse --git-dir 2> /dev/null)" ]]; then
+    local git_head git_state
+    git_head=$(sed -e 's,.*/\(.*\),\1,' <(git symbolic-ref HEAD 2>/dev/null || git rev-parse --short HEAD))
+    git_state=" $(git status --porcelain | sed -Ee 's/^(.M|M.|.R|R.) .*/\*/' -e 's/^(.A|A.) .*/\+/' -e 's/^(.D|D.) .*/\-/' | grep -oE '^(\*|\+|\?|\-)' | sort -u | tr -d '\n')"
+    _prompt_git_status=$git_head$git_state
+  else
+    _prompt_git_status=""
   fi
-  if [[ -n "$STY" ]]; then
-    if [[ -n "$env" ]]; then
-      env=+"+screen"
-    else
-      env="screen"
-    fi
+}
+
+function _prompt_generate_git {
+  if [[ -z "$_prompt_git_status" ]]; then
+    local -r path_color=$(print_color "$_color_path_bg")
+    _prompt_git="$_color_reset$path_color$_prompt_segment_char"
+  else
+    local -r path_git_color=$(print_color "$_color_path_bg" "$_color_git_bg")
+    local -r git_color=$(print_color "$_color_git_fg" "$_color_git_bg")
+    local -r git_end_color=$(print_color "$_color_git_bg")
+    _prompt_git="$path_git_color$_prompt_segment_char$git_color $_prompt_git_status $_color_reset$git_end_color$_prompt_segment_char"
   fi
 }
 
@@ -190,17 +205,17 @@ function _prompt_generate_alert {
 function set_prompt {
   _prompt_last_command_status="$?"
 
-  _prompt_generate_host
   _prompt_generate_chars
-  _prompt_generate_time
-  _prompt_generate_alert
+  _prompt_generate_host  
+  _prompt_generate_time  
   _prompt_generate_path
   _prompt_generate_git_status
   _prompt_generate_git
   _prompt_generate_filler
+  _prompt_generate_alert
   
   PS1="\n\[\033]0;\w\007\]$_prompt_host $_prompt_path $_prompt_git$_prompt_filler$_prompt_time\n $_prompt_ready_char$_color_reset "
   PS2="$(print_color $_color_prompt_ready) \$_prompt_ready_char $_color_reset"
 }
 
-[[ "$PROMPT_COMMAND" == *set_prompt* ]] ||  export PROMPT_COMMAND="set_prompt;$PROMPT_COMMAND"
+[[ "set_prompt;" =~ $PROMPT_COMMAND ]] ||  export PROMPT_COMMAND="set_prompt;$PROMPT_COMMAND"
